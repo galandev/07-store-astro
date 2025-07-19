681fa7f6990692c19013baa4194cd0273437cfe3
Roberto <rgalanllorca@me.com>
2025-07-19 13:15:03 +0200

    new changes

--- src/components/cart/CartCounter.tsx
+++ src/components/cart/CartCounter.tsx
@@ -1,9 +1,28 @@
+import { itemsInCart } from '@/store';
+import { CartCookiesClient } from '@/utils';
+import { useStore } from '@nanostores/react';
+import { useEffect } from 'react';
+
 export const CartCounter = () => {
+
+  const $itemsInCart = useStore(itemsInCart);
+
+  useEffect(() => {
+    const cart = CartCookiesClient.getCart();
+    itemsInCart.set(cart.length);
+  }, [])
+  
+
   return (
     <a href="/cart" className="relative inline-block">
-      <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5">
-        3
-      </span>
+
+      {
+        ($itemsInCart > 0) && (
+          <span className="absolute -top-2 -right-2 flex justify-center items-center bg-blue-600 text-white text-xs rounded-full w-5 h-5">
+            {$itemsInCart}
+          </span>
+        )
+      }
 
      <svg xmlns="http://www.w3.org/2000/svg" width={26} height={26} viewBox="0 0 24 24"><path fill="currentColor" d="M0 1h4.764l.545 2h18.078l-3.666 11H7.78l-.5 2H22v2H4.72l1.246-4.989L3.236 3H0zm7.764 11h10.515l2.334-7H5.855zM4 21a2 2 0 1 1 4 0a2 2 0 0 1-4 0m14 0a2 2 0 1 1 4 0a2 2 0 0 1-4 0"></path></svg>
     </a>
