41fa76a7f7f5950e1157105c8739ade1ce9747fc
Roberto <rgalanllorca@me.com>
2025-07-20 22:36:23 +0200

    add changes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export * from './products/ProductList';
 export * from './products/ProductCard';
 export * from './cart/CartCounter';
+export * from './products/ProductSlideshow.astro';
+export * from './products/ProductImage.astro';
